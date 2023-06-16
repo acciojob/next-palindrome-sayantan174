18989033b8b24a59887a8f26de2103e88956adc2
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-06-16 19:04:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function nextPalindrome(num) {
   //your JS code here.
+	function isPalindrome(num) {
+    return num.toString() === num.toString().split('').reverse().join('');
+  }
+
+  // Increment the number and check for palindrome until found
+  let nextNumber = number + 1;
+  while (!isPalindrome(nextNumber)) {
+    nextNumber++;
+  }
+
+  return nextNumber;
 }
 
-const input = prompt("Enter a palindrome number");
-alert(nextPalindrome(input));
+// const input = prompt("Enter a palindrome number");
+// alert(nextPalindrome(input));
